68c184ebba4cceec87ea3142e364295fa2d11186
ArcaLunar <arca@connect.hku.hk>
2025-02-10 09:48:07 +0800

    add some config (1)

---
+++ output/config/config.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+constexpr double kDegreeToRadian = 0.017453292519943295769236907684886;
+constexpr double kRadianToDegree = 57.295779513082320876798154814105;
+
+// Debug Options
+constexpr bool debug_mode = "debug";
