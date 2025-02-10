58aaee774dd406cb3eba3af07602dc5d406c3546
ArcaLunar <arca@connect.hku.hk>
2025-02-10 10:58:21 +0800

    impl armor (1), add -O3 compile flag

--- detector/include/armor.hpp
+++ detector/include/armor.hpp
@@ -1,8 +1,10 @@
 #ifndef __ARMOR_HPP__
 #define __ARMOR_HPP__
 
+#include <algorithm>
 #include <opencv2/core/types.hpp>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 namespace AutoAim {
 
@@ -11,7 +13,27 @@ namespace AutoAim {
  */
 struct LightBar : public cv::RotatedRect {
     double length, width;
+    double tilt_angle;
+    std::vector<cv::Point2f> points;
+    cv::Point2f top, bottom;
+
+    explicit LightBar() = default;
+    explicit LightBar(cv::RotatedRect &rect) : cv::RotatedRect{rect} {
+        cv::Point2f vertices[4];
+        rect.points(vertices);
+        std::copy(vertices, vertices + 4, points.begin());
+
+        std::sort(points.begin(), points.end(), [](const cv::Point2f &a, const cv::Point2f &b) {
+            return a.y < b.y;
+        });
+        top    = (points[0] + points[1]) / 2;
+        bottom = (points[2] + points[3]) / 2;
+        length = cv::norm(top - bottom);
+        width  = cv::norm(points[0] - points[1]);
+    }
 };
+
+struct Armor {};
 
 } // namespace AutoAim
 
