d33656ad493c8670d5798e5520c91f29bbe564ec
ArcaLunar <arca@connect.hku.hk>
2025-02-16 16:40:39 +0800

    classifier workflow

--- detector/include/classifier.hpp
+++ detector/include/classifier.hpp
@@ -1,9 +1,29 @@
 #ifndef __CLASSIFIER_HPP__
 #define __CLASSIFIER_HPP__
 
+#include "armor.hpp"
+
+#include <string>
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+
 namespace AutoAim {
 
-class Classifier {};
+class Classifier {
+  public:
+    Classifier(
+        const std::string &model_path, const std::string &label_path, const double &threshold,
+        const std::vector<std::string> &ignore = {}
+    );
+
+    void classify(std::vector<Armor> &armors);
+
+  private:
+    cv::dnn::Net net_;
+    std::vector<std::string> labels_, ignore_;
+    double threshold_;
+};
 
 } // namespace AutoAim
 
