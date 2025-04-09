42a4c37bd98de12aef90c03c899a3788e4784355
ArcaLunar <arca@connect.hku.hk>
2025-04-09 14:32:23 +0800

    add test for camera

---
+++ test/test_camera.cpp
@@ -0,0 +1,54 @@
+#include "camera.hpp"
+#include <atomic>
+#include <chrono>
+#include <opencv2/highgui.hpp>
+#include <spdlog/spdlog.h>
+
+volatile std::atomic_bool require_exit = false;
+
+void press_enter_to_exit() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        continue;
+
+    spdlog::error("[FROM main] Press Enter to exit");
+    while (getchar() != '\n')
+        continue;
+
+    require_exit = true;
+    sleep(1);
+}
+
+int main() {
+    HikCamera camera;
+
+    std::thread run([&] {
+        while (true) {
+            auto start_time     = std::chrono::system_clock::now();
+            RawImageFrame frame = camera.get_frame();
+
+            if (frame.image.empty()) {
+                spdlog::error("Failed to get frame");
+                continue;
+            }
+
+            // display image
+            cv::imshow("Camera", frame.image);
+            auto end_time = std::chrono::system_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+            spdlog::info("capture and display used {} ms", duration.count());
+
+            if (require_exit) {
+                spdlog::info("Exiting...");
+                break;
+            }
+        }
+    });
+
+    press_enter_to_exit();
+    run.join();
+
+    spdlog::info("exit successfully");
+    return 0;
+}
