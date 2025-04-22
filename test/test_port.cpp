343a8a7e3c84a9fe937572f70329bfc933dce4d3
ArcaLunar <arca@connect.hku.hk>
2025-04-22 19:41:16 +0800

    port test

--- test/test_port.cpp
+++ test/test_port.cpp
@@ -1,3 +1,15 @@
 #include "port.hpp"
+#include <thread>
 
-int main() { SerialPort port; }
+int main() {
+    SerialPort port("../../config/port.toml");
+
+    std::thread port_reader(&SerialPort::read_raw, &port);
+    std::thread port_processor(&SerialPort::process_raw, &port);
+    std::thread port_checker(&SerialPort::check_reconnect, &port);
+    port_reader.join();
+    port_processor.join();
+    port_checker.join();
+
+    return 0;
+}
