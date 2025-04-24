7307946cd51a2320de58a0d410b20212a56aab04
wsk12345 <wsk417@126.com>
2025-04-24 23:21:34 +0800

    annotator sturcture, align with existing code

--- detector/include/annotator.hpp
+++ detector/include/annotator.hpp
@@ -1,13 +1,29 @@
 #ifndef __ANNOTATOR_HPP__
 #define __ANNOTATOR_HPP__
 
+#include "classifier.hpp"
 #include "detector.hpp"
 #include "structs.hpp"
+#include <memory>
 #include <string>
+#include <vector>
 
 class Annotator {
   public:
     Annotator(std::string path);
+
+    /**
+     * @brief 根据摄像机拍到的图像和接受到的电控消息，进行标定、识别
+     * 
+     * @param frame 摄像机拍到的图像
+     * @param msg 电控消息
+     * @return std::vector<AnnotatedArmorInfo> 一帧图像中所有的装甲板信息
+     */
+    std::vector<AnnotatedArmorInfo> annotate(const RawImageFrame &frame, const RecvMsgType &msg);
+
+  private:
+    std::shared_ptr<OpenCVDetector> detector;
+    std::shared_ptr<Classifier> classifier;
 };
 
 #endif
--- detector/include/classifier.hpp
+++ detector/include/classifier.hpp
@@ -1,6 +1,51 @@
 #ifndef __CLASSIFIER_HPP__
 #define __CLASSIFIER_HPP__
 
-class Classifier {};
+#include "opencv2/dnn/dnn.hpp"
+#include "structs.hpp"
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+
+class Classifier {
+  public:
+    Classifier(std::string path);
+    /**
+     * @brief extract the class from ROI
+     *
+     * @param roi
+     * @return int representing the class
+     */
+    int classify(cv::Mat &roi);
+
+    /**
+     * @brief extract region of interest from the image
+     *
+     * @param img
+     * @param armor
+     * @return cv::Mat
+     */
+    cv::Mat extract_region_of_interest(const cv::Mat &img, const RawArmor &armor);
+    /**
+     * @brief Execute `extract_roi()` by batch
+     *
+     * @param img
+     * @param armors
+     * @return std::vector<cv::Mat>
+     */
+    std::vector<cv::Mat> extract_region_of_interest(const cv::Mat &img, const std::vector<RawArmor> &armors);
+
+    void load_labels(std::vector<int> &vec);
+
+  private:
+    cv::dnn::Net net;
+    std::vector<int> to_ignore, labels;
+    double confidence_threshold;
+
+  protected:
+    cv::Mat softmax(const cv::Mat &prob);
+    void preprocess(cv::Mat &src);
+    int inference(cv::Mat &src);
+};
 
 #endif
